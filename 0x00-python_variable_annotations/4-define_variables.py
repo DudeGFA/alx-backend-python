c15165ce6f25aa0396ae630ed5a2b30bc8d96b5d
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-03 16:25:00 +0100

    acreated 4-defibe_variables.py and 5-sum_list.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+contains variables a, pi,
+i_understand_annotations and school
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = true
+school: str = 'Holberton'
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import typing.List
+"""
+    contains function sum_list
+"""
+
+def sum_list(input_list: List[float]) -> float:
+    """
+        returns sum of all elements of a list
+    """
+    return sum(input_list)
