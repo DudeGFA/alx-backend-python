fb9e7f2dd6a5db13dfcb41d5dad8f8c8bf3a6ee5
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-03 13:10:17 +0100

    created 2 new files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Contains function add
+"""
+def add(a: int, b: int):
+    """
+        Args
+            a: int
+            b: int
+        Returns a + b
+    """
+    return a + b
