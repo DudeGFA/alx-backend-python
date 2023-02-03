db453d41ae83fe6383b1fb36808278b3f1312664
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-03 14:14:57 +0100

    created 4 new files
    created 2-main.py, 3-to_str.py, 3-main.py and 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+contains function floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+        returns integer part of a float
+        Arg
+            n: float
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    contains function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        returns string representaion of a float
+    """
+    return str(n)
