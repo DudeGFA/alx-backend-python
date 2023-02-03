e4f09856bc8dfd0e62c922f1a3e66f91f8de069f
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-03 13:58:36 +0100

    created 1-main.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    contains function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concats two strings
+        Args:
+            str1: str
+            str2: str
+        Return str1 + str2
+    """
+    return str1 + str2
