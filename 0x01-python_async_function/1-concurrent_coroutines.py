062209a92e1dfa774a9446360735f5eaa92d308b
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-06 18:04:17 +0100

    Tasks 2 and 3 solution

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,4 +15,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     listOfDelays = []
     for i in range(n):
         listOfDelays.append(await wait_random(max_delay))
-    return sort(listOfDelays)
+    return listOfDelays.sort()
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,2 +1,18 @@
-import time, asyncio
-__import__('0-basic_async_syntax')
+#!/usr/bin/env python3
+"""
+    Contains function measure_time
+"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+        measures and return time for execution
+        of function wait_n
+    """
+    s = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed = time.perf_counter() - s
+    return elapsed
